7608adc192021d543c777ce9f48f6c7570a845fe
Michael Ong <michaelong18@outlook.com>
2022-09-04 12:48:55 +0800

    Initial Commit

---
+++ yokongspam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yokongspam
+//
+//  Created by Michael Ong on 9/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
